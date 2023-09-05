f6a22a757c60faae5eaf29c8b51fdf1a5fa696d2
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-09-05 12:44:18 +0530

    Update alive.py

--- MukeshRobot/modules/alive.py
+++ MukeshRobot/modules/alive.py
@@ -19,7 +19,7 @@ PHOTO = [
 
 Mukesh = [
     [
-        InlineKeyboardButton(text="ɴᴏᴏʙ", user_id=OWNER_ID),
+        InlineKeyboardButton(text="⦏ʀ⦐⦏ᴅ⦐⦏x⦐", user_id=OWNER_ID),
         InlineKeyboardButton(text="ꜱᴜᴘᴘᴏʀᴛ", url=f"https://t.me/{SUPPORT_CHAT}"),
     ],
     [
